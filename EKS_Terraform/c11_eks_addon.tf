55cf3357e7eaa17ffccf4a7da6bc804292e24767
vanrajboricha <vanraj723@gmail.com>
2026-05-14 14:29:05 +0530

    Updating config

--- EKS_Terraform/c11_eks_addon.tf
+++ EKS_Terraform/c11_eks_addon.tf
@@ -8,3 +8,14 @@ resource "aws_eks_addon" "pod_identity_agent" {
 
   tags = var.tags
 }
+
+resource "aws_eks_addon" "aws-ebs-csi-driver" {
+  cluster_name = local.eks_cluster_name
+  addon_name   = "aws-ebs-csi-driver"
+
+  # Optional:
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  tags = var.tags
+}
