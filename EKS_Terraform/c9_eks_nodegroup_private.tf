02b72f13965b056f7c56ef08f3556e3370f66412
vanrajboricha <vanraj723@gmail.com>
2026-05-18 11:42:06 +0530

    adding files

--- EKS_Terraform/c9_eks_nodegroup_private.tf
+++ EKS_Terraform/c9_eks_nodegroup_private.tf
@@ -1,3 +1,43 @@
+#############################################
+# Launch Template for EC2 Worker Node Tags
+#############################################
+
+resource "aws_launch_template" "eks_nodes" {
+
+  name_prefix = "${local.name}-eks-node-lt"
+
+  #########################################
+  # ROOT VOLUME CONFIGURATION
+  #########################################
+
+  block_device_mappings {
+
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = var.node_disk_size
+      volume_type = "gp3"
+      encrypted   = true
+    }
+  }
+
+  #########################################
+  # TAGS FOR EC2 INSTANCES
+  #########################################
+
+  tag_specifications {
+
+    resource_type = "instance"
+
+    tags = merge(
+      var.tags,
+      {
+        Name = "${local.name}-eks-worker-node"
+      }
+    )
+  }
+  
+}
 resource "aws_eks_node_group" "private_nodes" {
   cluster_name = aws_eks_cluster.main.name
   node_group_name = "${local.name}-private-ng"
@@ -6,7 +46,15 @@ resource "aws_eks_node_group" "private_nodes" {
   instance_types = var.node_instance_types
   capacity_type = var.node_capacity_type
   ami_type = "AL2023_x86_64_STANDARD"
-  disk_size = var.node_disk_size
+  #disk_size = var.node_disk_size
+  #########################################
+  # ATTACH LAUNCH TEMPLATE
+  #########################################
+
+  launch_template {
+    id      = aws_launch_template.eks_nodes.id
+    version = "$Latest"
+  }
   scaling_config {
     desired_size = 3
     min_size = 1
